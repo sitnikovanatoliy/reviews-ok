8c67cb882acb44d9e9974e96f5167ae0ed0f20c7
sitnikovanatoliy <sitnikovanatoliy@gmail.com>
2025-05-08 13:27:29 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'reviews-ok',
-    script: 'app.js',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
+  apps: [
+    {
+      name: 'reviews-ok',
+      script: 'app.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        SESSION_SECRET: process.env.SESSION_SECRET || 'change_this_secret',
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+      }
     }
-  }]
+  ]
 };
