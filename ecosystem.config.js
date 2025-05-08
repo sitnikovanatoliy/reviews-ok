227b4b21a22c0c6606970656213412cd4f39014c
sitnikovanatoliy <sitnikovanatoliy@gmail.com>
2025-05-08 19:08:57 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
       name: 'reviews-ok',
       cwd: '/home/siteok/reviews-ok.online/www/reviews-ok',
       script: 'app.js',
+      // Логи приложения будут писаться сюда:
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
       env: {
         NODE_ENV: 'production',
         PORT: 3000
