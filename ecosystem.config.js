956ecef8a7f78e6678ed22a51ba6c9132ed911ec
sitnikovanatoliy <sitnikovanatoliy@gmail.com>
2025-05-08 14:43:34 +0300

    chore(pm2): add cwd to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'reviews-ok',
+      cwd: '/home/siteok/reviews-ok.online/www/reviews-ok', 
       script: 'app.js',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        SESSION_SECRET: process.env.SESSION_SECRET || 'change_this_secret',
+        SESSION_SECRET: process.env.SESSION_SECRET,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
       }
