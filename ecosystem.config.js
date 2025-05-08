c899c2e1b26ee3bca0386596a4de840fee8e8214
sitnikovanatoliy <sitnikovanatoliy@gmail.com>
2025-05-08 15:55:39 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,15 +5,8 @@ module.exports = {
       cwd: '/home/siteok/reviews-ok.online/www/reviews-ok',
       script: 'app.js',
       env: {
-        NODE_ENV: 'development',
-        PORT: 3000
-      },
-      env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,
-        SESSION_SECRET: process.env.SESSION_SECRET,
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+        PORT: 3000
       }
     }
   ]
